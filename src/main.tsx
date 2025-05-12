94286b1a8d4559614af6b2b78c536cbeac411ed2
bkcor23 <bkcor23@outlook.com>
2025-05-11 21:21:44 -0400

    fix main

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
+import App from "./App.tsx";
+import "./main.css";
 
-import { createRoot } from 'react-dom/client'
-import { Toaster } from 'sonner';
-import App from './App.tsx'
-import './main.css'
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <>
-    <App />
-    <Toaster position="top-right" richColors />
-  </>
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <Toaster position="top-right" richColors />
+    </QueryClientProvider>
+  </React.StrictMode>
 );
