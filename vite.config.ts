8018b69ef187e613c4325dff9d3a3f7cf165f4e2
bkcor23 <bkcor23@outlook.com>
2025-05-11 20:39:48 -0400

    index vite fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  // Set the base URL for GitHub Pages deployment
-  // In production, it will be set to the repository name
-  // In development, it will be "/"
-  base: mode === 'production' ? '/automated-finance-hub/' : '/',
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // Cambia a '/automated-finance-hub/' si tu app va en una subcarpeta
+})
